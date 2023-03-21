fa2654deda912543c641fb6119f76d783bc42bcc
Marianos96 <119233807+Marianos96@users.noreply.github.com>
2023-03-21 19:59:07 +0100

    task 2 MorseCodeDecoder and Encoder added

---
+++ src/main/java/MorseCoverter/MorseCodeDecoder.java
@@ -0,0 +1,13 @@
+package MorseCoverter;
+
+public class MorseCodeDecoder {
+    private final MorseCodeDictionary dictionary;
+
+    public MorseCodeDecoder(MorseCodeDictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public String decode(String morseCode){
+        return "";
+    }
+}
---
+++ src/main/java/MorseCoverter/MorseCodeEncoder.java
@@ -0,0 +1,13 @@
+package MorseCoverter;
+
+public class MorseCodeEncoder {
+    private final MorseCodeDictionary dictionary;
+
+    public MorseCodeEncoder(MorseCodeDictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public String decode(String text) {
+        return "";
+    }
+}
